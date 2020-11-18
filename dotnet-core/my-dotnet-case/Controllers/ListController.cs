7787ad585312f8cfb89c4cd1001a83e6b96c5e0a
StefanUnicus <64247207+StefanUnicus@users.noreply.github.com>
2020-11-18 15:22:01 +0100

    Add files via upload

---
+++ dotnet-core/my-dotnet-case/Controllers/ListController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_dotnet_case.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class ListController : ControllerBase
+    {
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+        [HttpGet]
+        public IActionResult getList() { return null; }
+
+        [HttpPost]
+        public IActionResult addListEntry() 
+        {
+            return null;
+        }
+    }
+}
---
+++ dotnet-core/my-dotnet-case/Models/Listentry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_dotnet_case.Models
+{
+    public class Listentry
+    {
+        private String Name { get; set; }
+        private int Price { get; set; }
+
+        public Listentry(String inputName, int inputPrice)
+        {
+            Name = inputName;
+            Price = inputPrice;
+        }
+    }
+}
